1169a5ffaa9324d17694e8b41b2393da8f6dbaf4
diegopr9 <diegopr9@hotmail.com>
2024-01-13 17:32:24 -0500

    Add files via upload

---
+++ AlumnosMasterData.js
@@ -0,0 +1,64 @@
+[{"alumno":"Pedro","nota":9.09,"ranking":10},{"alumno":"Juan","nota":4,"ranking":150},...]
+
+[
+   {
+      "alumno":"Diego Pachacama",
+      "nota":10.00,
+      "ranking":1,
+      "nsnc":302
+   },
+   {
+      "alumno":"Juan Diaz",
+      "nota":9.81,
+      "ranking":9,
+      "nsnc":260
+   },
+   {
+      "alumno":"Lisbet Vargas",
+      "nota":7,
+      "ranking":8,
+      "nsnc":14
+   },
+   {
+      "alumno":"Jose Padilla",
+      "nota":6,
+      "ranking":6,
+      "nsnc":1
+   },
+   {
+      "alumno":"Tomas Germano",
+      "nota":5.25,
+      "ranking":5,
+      "nsnc":160
+   },
+   {
+      "alumno":"Julio Palo",
+      "nota":4.14,
+      "ranking":4,
+      "nsnc":117
+   },
+   {
+      "alumno":"Marilin Manson",
+      "nota":4.10,
+      "ranking":3,
+      "nsnc":16
+   },
+   {
+      "alumno":"Silvestre Stalone",
+      "nota":3.92,
+      "ranking":2,
+      "nsnc":8
+   },
+   {
+      "alumno":"Osama Binladen",
+      "nota":3.33,
+      "ranking":1,
+      "nsnc":1
+   },
+   {
+      "alumno":"Pedro Torres",
+      "nota":2.94,
+      "ranking":0,
+      "nsnc":1
+   }
+]
